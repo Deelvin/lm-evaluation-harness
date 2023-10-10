264a5a526b7837e7839a81830092933c316ecf9e
Valery Chernov <valery.chernov@deelvin.com>
2023-10-10 13:23:32 +0400

    update deprecated urls

--- lm_eval/models/octoai_llms.py
+++ lm_eval/models/octoai_llms.py
@@ -13,21 +13,21 @@ model_urls = {
    "codellama-7b-instruct-mlc-q0f16": "https://codellama-7b-instruct-fp16-1gpu-g2ave3d5t9mm.octoai.run/v1/chat/completions",
    "codellama-7b-instruct-mlc-q4f16_1": "https://codellama-7b-instruct-int4-1gpu-g2ave3d5t9mm.octoai.run/v1/chat/completions",
    "codellama-7b-instruct-mlc-q8f16_1": "https://codellama-7b-instruct-int8-1gpu-g2ave3d5t9mm.octoai.run/v1/chat/completions",
-   "codellama-13b-instruct-mlc-q0f16": "https://codellama-13b-instruct-fp16-1gpu-g2ave3d5t9mm.octoai.run/v1/chat/completions",
+   "codellama-13b-instruct-mlc-q0f16": "https://codellama-13b-instruct-fp16-2gpu-g2ave3d5t9mm.octoai.run/v1/chat/completions",
    "codellama-13b-instruct-mlc-q4f16_1": "https://codellama-13b-instruct-int4-1gpu-g2ave3d5t9mm.octoai.run/v1/chat/completions",
    "codellama-13b-instruct-mlc-q8f16_1": "https://codellama-13b-instruct-int8-1gpu-g2ave3d5t9mm.octoai.run/v1/chat/completions",
-   "codellama-34b-instruct-mlc-q0f16": "https://codellama-34b-instruct-fp16-1gpu-g2ave3d5t9mm.octoai.run/v1/chat/completions",
+   "codellama-34b-instruct-mlc-q0f16": "https://codellama-34b-instruct-fp16-4gpu-g2ave3d5t9mm.octoai.run/v1/chat/completions",
    "codellama-34b-instruct-mlc-q4f16_1": "https://codellama-34b-instruct-int4-1gpu-g2ave3d5t9mm.octoai.run/v1/chat/completions",
-   "codellama-34b-instruct-mlc-q8f16_1": "https://codellama-34b-instruct-int8-1gpu-g2ave3d5t9mm.octoai.run/v1/chat/completions",
+   "codellama-34b-instruct-mlc-q8f16_1": "https://codellama-34b-instruct-int8-2gpu-g2ave3d5t9mm.octoai.run/v1/chat/completions",
    "llama2-7b-chat-mlc-q0f16": "https://llama2-7b-chat-fp16-1gpu-g2ave3d5t9mm.octoai.run/v1/chat/completions",
    "llama2-7b-chat-mlc-q4f16_1": "https://llama2-7b-chat-int4-1gpu-g2ave3d5t9mm.octoai.run/v1/chat/completions",
    "llama2-7b-chat-mlc-q8f16_1": "https://llama2-7b-chat-int8-1gpu-g2ave3d5t9mm.octoai.run/v1/chat/completions",
-   "llama2-13b-chat-mlc-q0f16": "https://llama2-13b-chat-fp16-1gpu-g2ave3d5t9mm.octoai.run/v1/chat/completions",
+   "llama2-13b-chat-mlc-q0f16": "https://llama2-13b-chat-fp16-2gpu-g2ave3d5t9mm.octoai.run/v1/chat/completions",
    "llama2-13b-chat-mlc-q4f16_1": "https://llama2-13b-chat-int4-1gpu-g2ave3d5t9mm.octoai.run/v1/chat/completions",
    "llama2-13b-chat-mlc-q8f16_1": "https://llama2-13b-chat-int8-1gpu-g2ave3d5t9mm.octoai.run/v1/chat/completions",
-   "llama2-70b-chat-mlc-q0f16": "https://llama2-70b-chat-fp16-4gpu-g2ave3d5t9mm.octoai.run/v1/chat/completions",
-   "llama2-70b-chat-mlc-q4f16_1": "https://llama2-70b-chat-int4-2gpu-g2ave3d5t9mm.octoai.run/v1/chat/completions",
-   "llama2-70b-chat-mlc-q8f16_1": "https://llama2-70b-chat-int8-4gpu-g2ave3d5t9mm.octoai.run/v1/chat/completions",
+   "llama2-70b-chat-mlc-q0f16": "https://llama2-70b-chat-fp16-4gpu-g2ave3d5t9mm.octoai.run/v1/chat/completions",    # +
+   "llama2-70b-chat-mlc-q4f16_1": "https://llama2-70b-chat-int4-2gpu-g2ave3d5t9mm.octoai.run/v1/chat/completions",  # -
+   "llama2-70b-chat-mlc-q8f16_1": "https://llama2-70b-chat-int8-4gpu-g2ave3d5t9mm.octoai.run/v1/chat/completions",  # -
    # TODO(vvchernov): it is demo, may be need to remove
    "llama-2-70b-chat": "https://llama-2-70b-chat-demo-kk0powt97tmb.octoai.run/v1/chat/completions",
 }
