4a2d53310098590995136398ed5e1ada07a85538
Valery Chernov <valery.chernov@deelvin.com>
2023-11-10 13:58:00 +0400

    refactor and clean code. Two runner for greedy_until and loglikelihood was implemented

--- lm_eval/models/octoai_llms.py
+++ lm_eval/models/octoai_llms.py
@@ -8,6 +8,212 @@ from lm_eval.base import BaseLM
 
 REPEAT_REQUEST_TO_OCTOAI_SERVER = 10
 
+
+class OctoAIEndpointRunnerBase():
+  url_postfix = None
+
+  def __init__(
+    self,
+    model_name: str="llama-2-70b-chat-int4",
+    url: str=None,
+    batch_size: int=1,
+    top_p: float=1.0,
+    temperature: float=0.0,
+    prod=True,
+    token=None,
+  ):
+    """
+    param model_name: str
+        Model name from the list of models supported by OctoAI
+    """
+    self.model_name = model_name
+    self.batch_size=batch_size
+    if url is not None:
+      self.url = url
+    else:
+      self.url = self.construct_request_url(prod)
+
+    self.init_msg_header(token)
+    self.init_base_msg(top_p, temperature)
+
+  def init_msg_header(self, token):
+    # Get the API key from the environment variables
+
+    if token is None: # there is no customized token, try to find in env
+      token = os.environ["OCTOAI_TOKEN"]
+      if token is None:
+        raise ValueError("TOKEN not found.")
+
+    self.headers = {
+      "authorization": f"Bearer {token}",
+      "content-type": "application/json",
+    }
+
+  def init_base_msg(self, top_p, temperature):
+    self.base_msg = {
+        "model": self.model_name,
+        "stream": False,
+        "max_tokens": 256,
+        "top_p": top_p,
+        "temperature": temperature,
+    }
+
+  def construct_request_url(self, prod):
+    if prod:
+      url = "https://text.octoai.run"
+    else:
+      url = "https://text.customer-endpoints.nimbus.octoml.ai"
+
+    return url
+
+  def get_base_msg(self):
+    return self.base_msg
+
+  def call_octoai_inference(self):
+    response = requests.post(self.url + self.url_postfix, headers=self.headers, json=self.msg)
+
+    if response.status_code != 200:
+      print(f"Error: {response.status_code} - {response.text}")
+
+    return json.loads(response.text)
+
+  def _batcher(self, requests):
+    for i in range(0, len(requests), self.batch_size):
+      yield requests[i:i + self.batch_size]
+
+  def run(self, requests, results):
+    if self.batch_size > 1:
+      for batch_idx, request_batch in enumerate(_batcher(requests)):
+        try:
+          self.model_generate_parallel(request_batch, results)
+        except ConnectionError as e:
+          print(f"ConnectionError: {e}. Skipping this batch and continuing...")
+        print(
+          f"\r{(batch_idx + 1) * self.batch_size}/{len(requests)} requests processed",
+          end="",
+        )
+    else:
+      for num, request in enumerate(requests):
+        try:
+          self.model_generate(request, results)
+        except ConnectionError as e:
+          print(f"ConnectionError: {e}. Skipping this request and continuing...")
+        print(f"\r{num}/{len(requests)} requests processed", end="")
+
+  def model_generate(self, request, results):
+    success = False
+    self.prepare_msg_data(request)
+    for _ in range(REPEAT_REQUEST_TO_OCTOAI_SERVER):
+      response = self.call_octoai_inference()
+      if 'choices' in response.keys():
+        success = True
+        break
+    if success:
+      results.append(self.get_result(response))
+    else:
+      print("ERROR: responce does not have choices. Dummy response was inserted")
+      results.append(self.dummy_result())
+
+  def model_generate_batch(self, request_batch, results):
+    parallel_results={}
+    for id in range(len(request_batch)):
+      parallel_results[id]=[]
+      self.model_generate(request_batch[id], parallel_results[id])
+
+    # Collect results together
+    for id in range(len(request_batch)):
+      results.extend(parallel_results[id])
+
+  def model_generate_parallel(self, request_batch, results):
+    import concurrent.futures
+
+    with concurrent.futures.ThreadPoolExecutor(max_workers=self.batch_size) as executor:
+      futures = []
+      parallel_results = {}
+      for id in range(len(request_batch)):
+        parallel_results[id]=[]
+        futures.append(executor.submit(self.model_generate, request_batch[id], parallel_results[id]))
+
+      for future in concurrent.futures.as_completed(futures):
+        try:
+          future.result()
+        except Exception as exc:
+          print(f"Error parallel generating predictions: {exc}")
+
+      # Collect results together
+      for id in range(len(request_batch)):
+        results.extend(parallel_results[id])
+
+  def get_result(self, response):
+    raise NotImplementedError("get_result method is not implemented in base class")
+
+  def dummy_result(self):
+    raise NotImplementedError("dummy_result method is not implemented in base class")
+
+
+class OctoAIEndpointRunnerGreedyUntil(OctoAIEndpointRunnerBase):
+  url_postfix = "/v1/chat/completions"
+
+  def __init__(self, **kwargs):
+    super().__init__(**kwargs)
+
+    self.msg = self.get_base_msg()
+
+  def prepare_msg_data(self, request):
+    inp = request[0]
+    # TODO(vvchernov): use until to init stop tokens
+    # request_args = request[1]
+    # until = request_args["until"]
+    self.msg["messages"] = [
+        {
+            "role": "user",
+            "content": inp,
+        }
+    ]
+
+  def get_result(self, response):
+    return response['choices'][0]['message']['content']
+
+  def dummy_result(self):
+    return "Dummy response"
+
+
+class OctoAIEndpointRunnerLogLikelihood(OctoAIEndpointRunnerBase):
+  url_postfix = "/v1/completions"
+
+  def __init__(self, **kwargs):
+    super().__init__(**kwargs)
+
+    self.msg = self.get_base_msg()
+    self.msg["logprobs"] = True
+    self.msg["prompt"] = "" # required parameter for completion request, do it dummy
+
+  def prepare_msg_data(self, request):
+    self.msg["context"] = request[0]
+    self.msg["continuation"] = request[1]
+
+  def get_result(self, response):
+    logprob_dict = json.loads(response['choices'][0]['text'])
+    logprob = logprob_dict["logprobes"]
+    is_greedy = logprob_dict["is_greedy"]
+    return (logprob, is_greedy)
+
+  def dummy_result(self):
+    import sys
+    return (-sys.float_info.max, False)
+
+
+runners = {
+  "greedy": OctoAIEndpointRunnerGreedyUntil,
+  "loglikelihood": OctoAIEndpointRunnerLogLikelihood,
+}
+
+def get_octoai_runner(runner_name: str):
+  if not runner_name in runners.keys():
+    raise ValueError(f"{runner_name} is not a name of octoai runner")
+  return runners[runner_name]
+
+
 class OctoAIEndpointLM(BaseLM):
   def __init__(
     self,
@@ -27,52 +233,27 @@ class OctoAIEndpointLM(BaseLM):
     """
     super().__init__()
 
+    # TODO(vvchernov): control it on high level
     self.time_meas = True
 
     self.model_name = model_name
-    self._batch_size = int(batch_size)
-    self.max_batch_size = max_batch_size
-    self._device = device
+    self._batch_size=int(batch_size)
+    self.max_batch_size=max_batch_size
+    self._device=device
 
-    self.init_remote(url, top_p, temperature, prod, token)
-
-  def init_remote(self, url, top_p, temperature, prod, token):
-    # Get the API key from the environment variables
-
-    if token is None: # there is no customized token, try to find in env
-      token = os.environ["OCTOAI_TOKEN"]
-      if token is None:
-        raise ValueError("TOKEN not found.")
-
-    if url is not None:
-      self.url = url
-    else:
-      self.url = self.construct_request_url(prod)
-
-    self.headers = {
-      "authorization": f"Bearer {token}",
-      "content-type": "application/json",
+    self.runner_args = {
+      "model_name": self.model_name,
+      "url": url,
+      "batch_size": self._batch_size,
+      "top_p": top_p,
+      "temperature": temperature,
+      "prod": prod,
+      "token": token,
     }
-
-    self.data = {
-        "model": self.model_name,
-        "stream": False,
-        "max_tokens": 256,
-        "top_p": top_p,
-        "temperature": temperature,
-    }
-
-  def construct_request_url(self, prod):
-    if prod:
-      url = "https://text.octoai.run"
-    else:
-      url = "https://text.customer-endpoints.nimbus.octoml.ai"
-
-    return url
 
   @property
   def eot_token_id(self):
-    raise NotImplementedError
+    raise NotImplementedError("No eot token is supported.")
 
   @property
   def max_length(self):
@@ -96,72 +277,12 @@ class OctoAIEndpointLM(BaseLM):
   def tok_decode(self, tokens):
     raise NotImplementedError("Cannot call tokenizer by API")
 
-  def loglikelihood(self, requests):
-    if not requests:
-      return []
-
+  def test(self, runner, requests):
     results = []
     if self.time_meas:
       start_timer = time.time()
-    if self.batch_size > 1:
-      def _batcher(in_requests):
-        for i in range(0, len(in_requests), self.batch_size):
-          yield in_requests[i:i + self.batch_size]
 
-      for request_batch in _batcher(requests):
-        try:
-          # TODO(vvchernov): Use _model_generate_parallel(...) when it becomes possible
-          self._model_generate_batch_for_loglikelihood(request_batch, results)
-        except ConnectionError as e:
-          print(f"ConnectionError: {e}. Skipping this batch and continuing...")
-    else:
-      for context, continuation in requests:
-        try:
-          self._model_generate_for_loglikelihood(context, continuation, results)
-        except ConnectionError as e:
-          print(f"ConnectionError: {e}. Skipping this request and continuing...")
-
-    if self.time_meas:
-      stop_timer = time.time()
-      secs = stop_timer - start_timer
-      print(
-        "Full time of predictions measurement: {:.2f} sec, {:.2f} min, {:.2f} hour(s)".format(
-            secs, secs / 60, secs / 3600))
-
-    return results
-
-  def greedy_until(self, requests):
-    if not requests:
-      return []
-
-    results = []
-    if self.time_meas:
-      start_timer = time.time()
-    if self.batch_size > 1:
-
-      def _batcher(in_requests):
-        for i in range(0, len(in_requests), self.batch_size):
-          yield in_requests[i : i + self.batch_size]
-
-      for batch_idx, request_batch in enumerate(_batcher(requests)):
-        try:
-          self._model_generate_parallel(request_batch, results)
-        except ConnectionError as e:
-          print(f"ConnectionError: {e}. Skipping this batch and continuing...")
-        print(
-          f"\r{(batch_idx + 1) * self.batch_size}/{len(requests)} requests processed",
-          end="",
-        )
-    else:
-      for num, request in enumerate(requests):
-        inp = request[0]
-        request_args = request[1]
-        until = request_args["until"]
-        try:
-          self._model_generate(inp, results, stop=until)
-        except ConnectionError as e:
-          print(f"ConnectionError: {e}. Skipping this request and continuing...")
-        print(f"\r{num}/{len(requests)} requests processed", end="")
+    runner.run(requests, results)
 
     if self.time_meas:
       stop_timer = time.time()
@@ -172,109 +293,19 @@ class OctoAIEndpointLM(BaseLM):
 
     return results
 
-  def call_octoai_reset(self):
-    try:
-      resp = requests.post(self.url + "/chat/reset", headers=self.headers)
-      return resp.json()
-    except Exception as e:
-      print(f"Error resetting chat for endpoint {self.url}")
-      print(e)
-      return
+  def accuracy_test(self, requests, runner_name="greedy"):
+    if not requests:
+      return []
 
-  def call_octoai_inference(self, url_postfix: str):
-    response = requests.post(self.url + url_postfix, headers=self.headers, json=self.data)
+    runner = get_octoai_runner(runner_name)(**self.runner_args)
 
-    if response.status_code != 200:
-      print(f"Error: {response.status_code} - {response.text}")
+    return self.test(runner, requests)
 
-    return json.loads(response.text)
+  def loglikelihood(self, requests):
+    return self.accuracy_test(requests, "loglikelihood")
+
+  def greedy_until(self, requests):
+    return self.accuracy_test(requests)
 
   def _model_call(self, inps):
     raise NotImplementedError("OctoAI does not support one model call")
-
-  def _model_generate(self, inps, results, stop=[]):
-    success = False
-    self.data["messages"] = [
-        {
-            "role": "user",
-            "content": inps,
-        }
-    ]
-    for _ in range(REPEAT_REQUEST_TO_OCTOAI_SERVER):
-      response = self.call_octoai_inference("/v1/chat/completions")
-      if 'choices' in response.keys():
-        success = True
-        break
-    if success:
-      results.append(response["choices"][0]["message"]["content"])
-    else:
-      print("ERROR: responce does not have choices. Dummy response was inserted")
-      results.append("Dummy response")
-
-  def _model_generate_batch(self, request_batch, results):
-    parallel_results = {}
-    for requiest_id in range(len(request_batch)):
-      parallel_results[requiest_id] = []
-      inp = request_batch[requiest_id][0]
-      request_args = request_batch[requiest_id][1]
-      until = request_args["until"]
-      self._model_generate(inp, parallel_results[requiest_id], stop=until)
-
-    # Collect results together
-    for requiest_id in range(len(request_batch)):
-      results.extend(parallel_results[requiest_id])
-
-  def _model_generate_for_loglikelihood(self, context, continuation, results):
-    success = False
-    self.data["context"] = context
-    self.data["continuation"] = continuation
-    for _ in range(REPEAT_REQUEST_TO_OCTOAI_SERVER):
-      response = self.call_octoai_inference("/v1/completions")
-      if 'choices' in response.keys():
-        success = True
-        break
-    if success:
-      logprob_dict = json.loads(response['choices'][0]['text'])
-      logprob = logprob_dict["logprobes"]
-      is_greedy = logprob_dict["is_greedy"]
-      results.append((logprob, is_greedy))
-    else:
-      print("ERROR: responce does not have choices. Dummy response was inserted")
-      import sys
-      results.append((-sys.float_info.max, False))
-
-  def _model_generate_batch_for_loglikelihood(self, request_batch, results):
-    parallel_results={}
-    for id in range(len(request_batch)):
-      parallel_results[id]=[]
-      context, continuation = request_batch[id]
-      self._model_generate_for_loglikelihood(context, continuation, parallel_results[id])
-
-    # Collect results together
-    for id in range(len(request_batch)):
-      results.extend(parallel_results[id])
-
-  def _model_generate_parallel(self, request_batch, results):
-    import concurrent.futures
-
-    with concurrent.futures.ThreadPoolExecutor(max_workers=self.batch_size) as executor:
-      futures = []
-      parallel_results = {}
-      for id in range(len(request_batch)):
-        parallel_results[id] = []
-        inp = request_batch[id][0]
-        request_args = request_batch[id][1]
-        until = request_args["until"]
-        futures.append(
-            executor.submit(self._model_generate, inp, parallel_results[id], stop=until)
-        )
-
-      for future in concurrent.futures.as_completed(futures):
-        try:
-          future.result()
-        except Exception as exc:
-          print(f"Error parallel generating predictions: {exc}")
-
-      # Collect results together
-      for id in range(len(request_batch)):
-        results.extend(parallel_results[id])
