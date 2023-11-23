981b14f64544b4938d9c804c868aa320a3502284
islavutin <iliya.slavutin@gmail.com>
2023-11-23 14:18:40 +0200

    minor changes to health check script

--- tests_ollm/healthcheck.py
+++ tests_ollm/healthcheck.py
@@ -11,20 +11,20 @@ init(autoreset=True)
 def fail_message(msg):
     print(Fore.RED + "✗", end=' ')  
     print(f"{msg}") 
-
+    
 def success_message(msg):      
     print(Fore.GREEN + "✓", end=' ')  # Print checkmark symbol in green
     print(f"{msg}")    
 
-def construct_request_url(url, model, prod):
-    return f"{url}/v1/chat/completions"
-
-def check_endpoint_health(endpoint, token, prod=True):
-
+def construct_request_url(endpoint):
+    return f"{endpoint['url']}/v1/chat/completions"
+    
+def check_endpoint_health(endpoint, token):
+    
     print(f'Model: {endpoint["model"]}')
 
     try:
-        request_url = construct_request_url(endpoint['url'], endpoint['model'],prod)
+        request_url = construct_request_url(endpoint)
         print(request_url)    
         response = requests.post(request_url, headers={'Content-Type': 'application/json', 'Authorization': f'Bearer {token}'}, json={
             "model": endpoint['model'],
@@ -48,44 +48,33 @@ def check_endpoint_health(endpoint, token, prod=True):
     except requests.RequestException as e:
         fail_message(f"Basic request")
 
-def check_all_endpoints(endpoints, token, prod):
+def check_all_endpoints(endpoints, token):
     for endpoint in endpoints:
-        check_endpoint_health(endpoint, token, prod)
+        check_endpoint_health(endpoint, token)
         print()  # Add a new line for better readability between endpoint reports
 
 def parse_arguments():
     parser = argparse.ArgumentParser(description='Check endpoint health')
-    parser.add_argument('--prod', action='store_true', default=False, help='Flag to indicate production environment')
     return parser.parse_args()
 
 
 if __name__ == "__main__":
     args = parse_arguments()
-
+    
     # Fetch OCTOAI_TOKENs from environment variable
     octoai_token_dev = os.getenv('OCTOAI_TOKEN_DEV')
     octoai_token_prod = os.getenv('OCTOAI_TOKEN_PROD')
-
-    if (args.prod == True):
-        if octoai_token_prod is None:
-            print(Fore.RED + "OCTOAI_TOKEN_PROD environment variable not found.")
-            exit(1)
-        else:
-            octoai_token = octoai_token_prod
-    else:
-        if octoai_token_dev is None:
-            print(Fore.RED + "OCTOAI_TOKEN_DEV environment variable not found.")
-            exit(1)
-        else:
-            octoai_token = octoai_token_dev
-
+    
     # Load endpoint details from JSON file
     with open('endpoints.json', 'r') as file:
-        data = json.load(file)
-        if (args.prod):
-            endpoints = data.get('prod', [])
-        else:
-            endpoints = data.get('dev', [])
+        endpoints = json.load(file)
 
-    # Check health for all endpoints
-    check_all_endpoints(endpoints, octoai_token, args.prod)
+    if octoai_token_prod is None:
+        print(Fore.RED + "OCTOAI_TOKEN_PROD environment variable not found.")
+    else:
+        check_all_endpoints(endpoints["prod"], octoai_token_prod)
+
+    if octoai_token_dev is None:
+        print(Fore.RED + "OCTOAI_TOKEN_DEV environment variable not found.")
+    else:
+        check_all_endpoints(endpoints["dev"], octoai_token_dev)
