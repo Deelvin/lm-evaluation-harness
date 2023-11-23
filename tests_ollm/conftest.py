92f9196a3606a65464d0143e078a8a23991745c6
Valery Chernov <valery.chernov@deelvin.com>
2023-11-23 12:13:07 +0400

    upstream with ollm from last main

--- tests_ollm/conftest.py
+++ tests_ollm/conftest.py
@@ -1,12 +1,4 @@
 def pytest_addoption(parser):
-    parser.addoption("--model_name", required=True, type=str, action="store")
-    parser.addoption("--endpoint", required=True, type=str, action="store")
+    parser.addoption("--model_name", default=None, type=str, action="store")
+    parser.addoption("--endpoint", default=None, type=str, action="store")
     parser.addoption("--context_size", type=int, default=2048, action="store")
-
-
-def pytest_generate_tests(metafunc):
-    # This is called for every test. Only get/set command line arguments
-    # if the argument is specified in the list of test "fixturenames".
-    option_value = metafunc.config.option.model_name
-    if "model_name" in metafunc.fixturenames and option_value is not None:
-        metafunc.parametrize("model_name", [option_value])
