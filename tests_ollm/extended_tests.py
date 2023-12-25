53c05f06627e1536d55b2d3503b4f87742a72f43
Red-Caesar <suslovavd2002@mail.ru>
2023-12-25 15:40:28 +0300

    add stress tests for large contents

--- tests_ollm/extended_tests.py
+++ tests_ollm/extended_tests.py
@@ -177,3 +177,89 @@ def test_send_many_request(num_workers, model_name, token, endpoint):
             responses_code_set.add(future.result().status_code)
 
     assert responses_code_set == {200}
+
+
+@pytest.mark.parametrize("num_workers", [64, 128])
+@pytest.mark.parametrize("input_tokens", [2031, 3957])
+def test_send_many_large_input_content(num_workers, input_tokens, model_name, context_size, token, endpoint):
+    if input_tokens >= context_size:
+        pytest.skip("Input tokens are bigger than context_size")
+
+    with open(path_to_file(f"input_context/text_about_{input_tokens}_tokens.txt"), "r") as file:
+        prompt = file.read()
+    request = model_data(model_name, prompt, max_tokens=1)
+    url = endpoint + "/v1/chat/completions"
+    headers = {
+        "Content-Type": "application/json",
+        "Authorization": f"Bearer {token}",
+    }
+    responses_code_set = set()
+        
+    with ThreadPoolExecutor(max_workers=num_workers) as executor:
+        futures = [executor.submit(send_request_get_response, url, request, headers) for _ in range(num_workers)]
+        for future in concurrent.futures.as_completed(futures):
+            responses_code_set.add(future.result().status_code)
+
+    assert responses_code_set == {200}
+
+
+@pytest.mark.parametrize("num_workers", [64, 128])
+def test_send_small_and_many_big_input_contents(num_workers, model_name, context_size, token, endpoint):
+    with open(path_to_file(f"input_context/text_about_496_tokens.txt"), "r") as file:
+        small_prompt = file.read()
+
+    if context_size >= 4096:
+        input_tokens = 3957
+    else:
+        input_tokens = 2031
+
+    with open(path_to_file(f"input_context/text_about_{input_tokens}_tokens.txt"), "r") as file:
+        big_prompt = file.read() 
+
+    small_request = model_data(model_name, small_prompt, max_tokens=10)
+    big_request = model_data(model_name, big_prompt, max_tokens=10)
+    url = endpoint + "/v1/chat/completions"
+    headers = {
+        "Content-Type": "application/json",
+        "Authorization": f"Bearer {token}",
+    }
+    responses_code_set = set()
+        
+    with ThreadPoolExecutor(max_workers=num_workers) as executor:
+        futures = [executor.submit(send_request_get_response, url, small_request, headers)]
+        futures += [executor.submit(send_request_get_response, url, big_request, headers) for _ in range(num_workers)]
+        for future in concurrent.futures.as_completed(futures):
+            responses_code_set.add(future.result().status_code)
+
+    assert responses_code_set == {200}
+
+
+def test_send_increasing_sequence_of_contents(model_name, context_size, token, endpoint):
+    input_tokens = [496, 963, 2031, 3119, 3957, 5173]
+    prompts = []
+    for input in input_tokens:
+        if input > context_size:
+            break
+        with open(path_to_file(f"input_context/text_about_{input}_tokens.txt"), "r") as file:
+            prompts.append(file.read()) 
+
+    requests = []
+    for i, prompt in enumerate(prompts):
+        request = model_data(model_name, prompt, max_tokens=10)
+        how_many = (i + 1) * 5
+        requests += [request] * how_many
+    num_workers = len(requests)
+
+    url = endpoint + "/v1/chat/completions"
+    headers = {
+        "Content-Type": "application/json",
+        "Authorization": f"Bearer {token}",
+    }
+    responses_code_set = set()
+
+    with ThreadPoolExecutor(max_workers=num_workers) as executor:
+        futures = [executor.submit(send_request_get_response, url, request, headers) for request in requests]
+        for future in concurrent.futures.as_completed(futures):
+            responses_code_set.add(future.result().status_code)
+
+    assert responses_code_set == {200}
