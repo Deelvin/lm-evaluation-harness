8f4d2f8c3032626f6880a208c112fd63d873bbe9
dsbarinov1 <dsbarinov_1@edu.hse.ru>
2023-12-28 09:46:22 +0000

    Add endpoints dictionary + fix paths

--- run_docker_tests.py
+++ run_docker_tests.py
@@ -2,23 +2,69 @@ import json
 import subprocess
 import os
 
+endpoints_data = {
+    "Dev": [
+        {
+            "url": "https://text.customer-endpoints.nimbus.octoml.ai",
+            "model": "codellama-7b-instruct-fp16",
+        },
+        {
+            "url": "https://text.customer-endpoints.nimbus.octoml.ai",
+            "model": "codellama-13b-instruct-fp16",
+        },
+        {
+            "url": "https://text.customer-endpoints.nimbus.octoml.ai",
+            "model": "codellama-34b-instruct-int4",
+        },
+        {
+            "url": "https://text.customer-endpoints.nimbus.octoml.ai",
+            "model": "codellama-34b-instruct-fp16",
+        },
+        {
+            "url": "https://text.customer-endpoints.nimbus.octoml.ai",
+            "model": "llama-2-13b-chat-fp16",
+        },
+        {
+            "url": "https://text.customer-endpoints.nimbus.octoml.ai",
+            "model": "llama-2-70b-chat-int4",
+        },
+        {
+            "url": "https://text.customer-endpoints.nimbus.octoml.ai",
+            "model": "llama-2-70b-chat-fp16",
+        },
+        {
+            "url": "https://text.customer-endpoints.nimbus.octoml.ai",
+            "model": "mistral-7b-instruct-fp16",
+        },
+        {
+            "url": "https://text.customer-endpoints.nimbus.octoml.ai",
+            "model": "mixstral-8x7b-instruct-fp16",
+        },
+    ],
+    "Prod": [
+        {"url": "https://text.octoai.run", "model": "codellama-7b-instruct-fp16"},
+        {"url": "https://text.octoai.run", "model": "codellama-13b-instruct-fp16"},
+        {"url": "https://text.octoai.run", "model": "codellama-34b-instruct-int4"},
+        {"url": "https://text.octoai.run", "model": "codellama-34b-instruct-fp16"},
+        {"url": "https://text.octoai.run", "model": "llama-2-13b-chat-fp16"},
+        {"url": "https://text.octoai.run", "model": "llama-2-70b-chat-int4"},
+        {"url": "https://text.octoai.run", "model": "llama-2-70b-chat-fp16"},
+        {"url": "https://text.octoai.run", "model": "mistral-7b-instruct-fp16"},
+    ],
+}
+
 token = os.getenv("OCTOAI_TOKEN")
 current_directory = os.getcwd()
 path = "test_results"
-# Читаем endpoints.json
-with open("endpoints.json", "r") as f:
-    endpoints_data = json.load(f)
 
 if not os.path.exists(os.path.join(current_directory, path)):
     os.makedirs(path)
 else:
     os.remove(os.path.join(path, "test_results.csv"))
 
-# Проходим по каждой модели
 for model_info in endpoints_data["Prod"]:
     model_name = model_info["model"]
-    # Строим команду docker run
+
     docker_command = f"docker run -v {current_directory}:/lm_eval/test_results -e OCTOAI_TOKEN={token} lm-eval pytest tests/unittest_endpoint.py -vv --model_name {model_name} --endpoint Prod"
 
-    # Выполняем команду
     subprocess.run(docker_command, shell=True)
