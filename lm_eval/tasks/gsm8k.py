05fcd28c2afed924b0183f45944bec89762f724d
Ailurus1 <kozulin02@yandex.ru>
2024-02-13 22:29:30 +0300

    Fix regexp matching

--- lm_eval/tasks/gsm8k.py
+++ lm_eval/tasks/gsm8k.py
@@ -87,9 +87,8 @@ class GradeSchoolMath8K(Task):
     def _extract_answer(self, completion):
         match = ANS_RE.search(completion)
         if match:
-            # match_str = match.group(1).strip()
-            # match_str = match_str.replace(",", "")
-            match_str = match.group(0)
+            match_str = match.group(1).strip()
+            match_str = match_str.replace(",", "")
             return match_str
         else:
             return INVALID_ANS
